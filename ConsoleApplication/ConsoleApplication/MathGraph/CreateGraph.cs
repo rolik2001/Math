c91cabb1a83516db9d315927da45379b21e4f6fc
rolik2001 <roman.vaseruk@gmail.com>
2017-12-27 19:22:28 +0200

    first idea

---
+++ ConsoleApplication/ConsoleApplication/MathGraph/CreateGraph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication.MathGraph
+{
+        public class CreateGraph
+        {
+            private static List<Node> _listNode = new List<Node>();
+            private static Random _random = new Random();
+        
+            public static void Start()
+            {
+                var startNode = new Node(_random);
+                _listNode.Add(startNode);
+                for (var i = 0; i < 9; i++)
+                {
+                    CreateX();
+                }
+            }
+
+            private static void CreateX()
+            {
+//            var x = new Node();
+            }
+        }
+    }
---
+++ ConsoleApplication/ConsoleApplication/MathGraph/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication.MathGraph
+{
+    public class Node
+        {
+            public string Id;
+            public string Type;
+            public int Position;
+        
+            public Node(string id)
+            {
+                Id = id;
+            }
+
+            public Node(Random random)
+            {
+                Random r = random;
+                int len = random.Next(2, 10);
+                string[] consonants =
+                {
+                    "a","b", "c", "d", "e", "f", "g", "h", "j","i", "k", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t","u", "v",
+                    "w", "x","y","z"
+                };
+                string[] vowels = {"1","2","3","4","5","6","7","8","9"};
+                string Name = "";
+                Name += consonants[r.Next(consonants.Length)].ToUpper();
+                Name += vowels[r.Next(vowels.Length)];
+                int b = 2;
+                while (b < len)
+                {
+                    Name += consonants[r.Next(consonants.Length)];
+                    b++;
+                    Name += vowels[r.Next(vowels.Length)];
+                    b++;
+                }
+            
+                Console.WriteLine(Name);   
+                Id = Name ;
+            }
+        }
+    }
+
+
